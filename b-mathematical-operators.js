edd194915903a7213fbb51e557b8df8506ad51d6
Quinten van Hoefen Wijsard <quinten@MacBook-Pro-van-Quinten.local>
2021-01-23 14:30:16 +0100

    huiswerk update 2

--- b-mathematical-operators.js
+++ b-mathematical-operators.js
@@ -5,27 +5,40 @@
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9
 // ==========================================
-
+const autoRood = 4
+const autoBlauw = 5
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364
 // ==========================================
-
+const aantalJaar = 7
+const aantalWeek = 52
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49
 // ==========================================
+const A = 4
+const B = 3
+const aantalDagen = 7
+const totaal3 = (A+B)*7
 
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12
 // ==========================================
+const autoA = 36
+const autoB = 6
+const multiplyer = 2
+const totaal4 = (autoA/autoB)*2
 
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig'
 // ==========================================
+const smaakA = "zoet"
+const smaakB = "sappig"
+const flavour = smaakA + smaakB
 
 
 // ==========================================
@@ -33,10 +46,16 @@
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters'
 // ==========================================
-
+const voornaam = "Quinten"
+const achternaam = "van Hoefen Wijsard"
+const naam = voornaam + ' ' + achternaam
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg
 // ==========================================
+const woonplaats = "Utrecht"
+const provincie = "Utrecht"
+const adres = woonplaats + ',' + ' ' + provincie
+console.log(adres)
